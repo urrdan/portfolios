8306119ef1cc4792ebe4bdc19caf44c41cbe4872
Mohamed Abdulle <yarae99@gmail.com>
2019-06-21 18:48:40 -0500

    branch changes 1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){ 
+
     lst=['creator','programmer','coder','designer']
     var play=0
     setInterval(function(){
@@ -8,24 +9,34 @@ $(document).ready(function(){
     },2000)
     
     $('.menu-icon').click(function(){
-        $('#hidden-menu').toggleClass('show-menu hidden-menu')
-        $('.fas').toggleClass('fa-times')
-        $('.menu-bar').toggleClass('visible')
-        
-        
+        $(this).toggleClass('fa-bars fa-times')
+        $('.sidebar').toggleClass('sidebar-visible') 
+    })
+    $('.sidebar').click(function(){
+        $('.menu-icon').removeClass('fa-times').addClass('fa-bars')
+        $('.sidebar').removeClass('sidebar-visible') 
     })
     
     $(window).scroll(function(){
-        if($(window).scrollTop()<$(window).height()){
+        if($(window).scrollTop()<$('.welcomePage').outerHeight()){
             $('.menu-bar').css('display','none')
+            $('.sidebar-cont').css('opacity','0')
+            console.log('lolo')
         }else{
             $('.menu-bar').css('display','initial')
+            cloned=$('.welcomePage').children('.logoo,ul')
+            $('.sidebar-cont').css({'opacity':'1'})
+            if( $('.sidebar').children().length<2){
+                $('.sidebar').append(cloned.clone())
+            }
         }
         //find away to improve the code below i.e to shorten it
         //code for hidding menu when scrolling
-        $('#hidden-menu').removeClass('show-menu').addClass('hidden-menu')
+        /* $('#hidden-menu').removeClass('show-menu').addClass('hidden-menu')
         $('.menu-bar').addClass('visible')
-        $('.fas').removeClass('fa-times').addClass('fa-bars')
+        $('.fas').removeClass('fa-times').addClass('fa-bars') */
         
     })
+    
+    $('.sidebar').height($(window).height())
 })
