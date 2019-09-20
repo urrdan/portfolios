4ff187803976fd05cbe9a4f107421867bb09414b
Mohamed Abdulle <yarae99@gmail.com>
2019-09-20 10:07:03 -0500

    menu dropdown redesign

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $('.small-sidebar').css('display','none') 
     $('.dimensions').fadeOut(0)
 
-    //testing
+    //adding menu to small screens
     cloned=$('.welcomePage').children('.logoo,ul')
     cloned2=$('.side-contact')
     $('.sidebars').append(cloned.clone())
@@ -20,6 +20,7 @@ $(document).ready(function(){
     $('.small-sidebar').append(cloned2.clone())
 
     //functions
+
     function scrollanime(ele,classtoadd){
         //for animating element on scroll
         if($(window).scrollTop()>($(ele).offset().top - $(window).innerHeight()+$(ele).outerHeight()/2 )){
@@ -33,7 +34,7 @@ $(document).ready(function(){
         if (play>3){play=0}
     },2000)
 
-    //for the lines on either side of heading
+    //for the lines on either side of headings
     $('.heading-cont').append('<div><div>').prepend('<div>')
     $('.heading-cont').children('div').addClass('heading-div')
 
@@ -41,37 +42,29 @@ $(document).ready(function(){
     $('.menu-icon').click(function(){
         $(this).toggleClass('fa-bars fa-times')
         $('.small-sidebar').slideToggle()
-        //console.log('menubar')
     })
     
     $('.small-sidebar').click(function(){
         $('.menu-icon').removeClass('fa-times').addClass('fa-bars')
-        $('.small-sidebar').slideUp()
+        $('.small-sidebar').css('display','none')
     })
 
-    //nothing important (for dimensions)
+    //nothing important (just for dimensions)
     $('.comment:last >i').click(function(){
         $('.dimensions').fadeToggle()
     })
 
     //windowscroll
+    $(window).scroll(function(){console.log($())})
 
     $(window).scroll(function(){
         $('.sidebars').height($(window).height() - $('.smll-scrn-menu').height())
 
-        if($(window).scrollTop()<($('.welcomePage').outerHeight()-30)){
+        if($(window).scrollTop()<($('.welcomePage').outerHeight())){
             //$('.menu-bar').css('display','none')//delete
             $('.sidebar-cont').css('opacity','0')
         }else{
             $('.sidebar-cont').css({'opacity':'1'})
-            /* cloned=$('.welcomePage').children('.logoo,ul')
-            cloned2=$('.side-contact')
-            
-            if( $('.sidebars').children().length<2){
-                $('.sidebars').append(cloned.clone())
-                $('.sidebars>ul').prepend("<a href='#welcomePage'><li>HOME</li></a>")
-                $('.small-sidebar').append(cloned2.clone())
-            } */
         
         }
         scrollanime('.proj:eq(0)','projsudoclassafter')
@@ -90,7 +83,7 @@ $(document).ready(function(){
         $(this).siblings('.see-moreOrHide').fadeIn()
     })
 
-    //needs correction 2 problems //-1
+
     $('.sidebars').height($(window).height() - $('.smll-scrn-menu').height())
 
     //dimension
